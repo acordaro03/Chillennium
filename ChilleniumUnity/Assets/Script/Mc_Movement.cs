54f44181e0a404387c72711b6acab05409d2eaf2
SarahBesserer <35742290+sjb160130@users.noreply.github.com>
2018-10-12 19:04:16 -0500

    Movement

--- ChilleniumUnity/Assets/Script/Mc_Movement.cs
+++ ChilleniumUnity/Assets/Script/Mc_Movement.cs
@@ -1,25 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mc_Movement : MonoBehaviour {
-
-	public float Mc_speed;
-
-    public Rigidbody2D Mc_rb;
+public class Mc_Movement : MonoBehaviour
+{
+    public float speed = 1.5f;
+    private Rigidbody2D rb;
 
     void Start()
     {
-        Mc_rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    private void Update()
     {
-        float movementHori = Input.GetAxis("Horizontal");
-        float movementVerti = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(movementHori, 0.0f, movementVerti);
-
-        Mc_rb.AddForce(movement * Mc_speed);
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * (speed +2)  * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
     }
 }
+
