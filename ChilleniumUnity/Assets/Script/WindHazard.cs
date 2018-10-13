9326ac1af39153d5c839d904e638e487ce961fa7
acordaro03 <35272959+acordaro03@users.noreply.github.com>
2018-10-12 20:14:10 -0500

    WindHazard

---
+++ ChilleniumUnity/Assets/Script/WindHazard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindHazard : MonoBehaviour {
+    GameObject Mc;
+    public float windSheer = 1f;
+
+    void Start()
+    {
+        GameObject Mc = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Mc_Movement mc_Movement = GetComponent<Mc_Movement>();
+        mc_Movement.speed -= windSheer;
+    }
+}
+
+
